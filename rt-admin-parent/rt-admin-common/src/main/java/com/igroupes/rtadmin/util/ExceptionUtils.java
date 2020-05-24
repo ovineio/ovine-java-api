370f9b16d25cc679e39b3fb9648b1b127f5e99c0
zhangxiqiang <2291859802@qq.com>
2020-05-24 23:23:39 +0800

    参数校验异常统一处理

--- rt-admin-parent/rt-admin-common/src/main/java/com/igroupes/rtadmin/util/ExceptionUtils.java
+++ rt-admin-parent/rt-admin-common/src/main/java/com/igroupes/rtadmin/util/ExceptionUtils.java
@@ -2,8 +2,13 @@ package com.igroupes.rtadmin.util;
 
 import com.igroupes.rtadmin.enums.ErrorCode;
 import com.igroupes.rtadmin.file.exception.BusinessException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Set;
 
 public class ExceptionUtils {
     private ExceptionUtils() {
@@ -13,9 +18,22 @@ public class ExceptionUtils {
         if (exception instanceof BusinessException) {
             return (BusinessException) exception;
         } else if(exception instanceof ConstraintViolationException) {
-            ConstraintViolationException ex = (ConstraintViolationException)exception;
+            ConstraintViolationException cve = (ConstraintViolationException)exception;
+            Set<ConstraintViolation<?>> cves = cve.getConstraintViolations();
+            StringBuffer errorMsg = new StringBuffer();
+            cves.forEach(ex -> errorMsg.append(ex.getMessage()).append(";"));
             ErrorCode ec = ErrorCode.PARAM_ERROR;
-            ec.setMessage(ex.getMessage());
+            ec.setMessage(errorMsg.toString());
+            return new BusinessException(ec);
+        }else if(exception instanceof MethodArgumentNotValidException) {
+            MethodArgumentNotValidException c = (MethodArgumentNotValidException) exception;
+            List<ObjectError> errors = c.getBindingResult().getAllErrors();
+            StringBuffer errorMsg = new StringBuffer();
+            errors.stream().forEach(x -> {
+                errorMsg.append(x.getDefaultMessage()).append(";");
+            });
+            ErrorCode ec = ErrorCode.PARAM_ERROR;
+            ec.setMessage(errorMsg.toString());
             return new BusinessException(ec);
         }else{
             return new BusinessException(ErrorCode.SYSTEM_ERROR);
