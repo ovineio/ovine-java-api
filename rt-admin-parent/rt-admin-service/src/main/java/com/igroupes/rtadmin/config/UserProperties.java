ae9323e67329f2d84691e5c655b8a832e7be50f5
from000 <fromzero_best@sina.com>
2020-06-06 22:22:38 +0800

    添加用户默认头像

---
+++ rt-admin-parent/rt-admin-service/src/main/java/com/igroupes/rtadmin/config/UserProperties.java
@@ -0,0 +1,12 @@
+package com.igroupes.rtadmin.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "rtadmin.user")
+public class UserProperties {
+    private String defaultAvatar;
+}
